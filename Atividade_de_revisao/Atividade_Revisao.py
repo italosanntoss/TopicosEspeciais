ce9823772f7fb6f29ee77d49639fe3fc91a67f58
IFPB <italo999santos@gmail.com>
2019-04-12 08:53:33 -0300

    Exercicio 1 de TE

---
+++ Atividade_de_revisao/Atividade_Revisao.py
@@ -0,0 +1,89 @@
+Atividade DE REVISÃO:
+Questão 1-
+t = 0
+z = 0
+enquanto i <6:
+n = flutuador (entrada ())
+i + 1 =
+se n> 0:
+t + 1 =
+Se n == 0:
+t = t + 0
+print ( "% d Valores positivos" % (t))
+
+Questão 2-
+Vpo = lista ()
+VNe = lista ()
+para i na faixa (6):
+valentes = int (entrada ())
+if (valor <0): ln.append (valor)
+outra coisa: vp.append (valor)
+print ( "Valores Positivos:", VPO)
+print ( "Valores Negativos:", VNe)
+
+Questão 3
+b = int (entrada ())
+para i na gama (n):
+b1 = flutuador (entrada ())
+b2 = flutuador (entrada ())
+b3 = flutuador (entrada ())
+
+de impressão (((b1 * 2) + (b2 * 3) + (b3 * 5)) / 10)
+
+Questão 4-
+po = 0
+MA = 0
+para i no intervalo (15)
+n = int (entrada ())
+
+se n> Maior:
+MA = n
+po = i + 1
+
+print ( "d \ n% d" % (ma, po))
+
+Questão 5-
+Lista = lista ()
+para i na faixa (6):
+valentes = flutuador (entrada ())
+if (valor> = 1):
+lista.append (VA)
+soma = soma (l)
+print ( 'media dos Valores positivos:% .1f' % (soma / len (Lista)))
+
+Questão 6-
+
+QuantidadePizza = int (entrada ())
+PrecoPizza = flutuador (entrada ())
+PrecoTotal = QuantidadePizza * PrecoPizza
+Imposto = 8 * prcPizza / 100
+Total = PrecoPizza + Imposto
+print (total)
+
+Questão 7-
+
+VLR = int (entrada ( "informe o valor:"))
+ttl = lista ()
+ttln = 1
+
+para i na gama (1, VLR + 1,1):
+ttln = ttln * i
+
+print (ttln)
+
+Questão 8-
+
+b = int (entrada ( "informe uma base:"))
+e = int (entrada ( "informe o Expoente:"))
+
+print (b ** e)
+
+
+Questão 9
+
+a = int (entrada ())
+Soma = 0
+para i na faixa de (1, N):
+soma + = i
+
+print (soma) 
